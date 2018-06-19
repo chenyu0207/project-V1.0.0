c662cf1e4a6defb96f74ce94e5c14dc76e5fc361
47.105.99.15 <cuyc0207@163.com>
2018-06-19 16:18:37 +0800

    hebing

--- www/Application/User/Conf/config.php
+++ www/Application/User/Conf/config.php
@@ -15,5 +15,5 @@
 define('UC_APP_ID', 1); //应用ID
 define('UC_API_TYPE', 'Model'); //可选值 Model / Service
 define('UC_AUTH_KEY', '2Bjm%Q_c6K|>J<`IkU/NwvCr&DFAM=8@E-h:3}9x'); //加密KEY
-define('UC_DB_DSN', 'mysqli://root:root@localhost:3306/project'); // 数据库连接，使用Model方式调用API必须配置此项
+define('UC_DB_DSN', 'mysqli://root:root@127.0.0.1:3306/project'); // 数据库连接，使用Model方式调用API必须配置此项
 define('UC_TABLE_PREFIX', 'pro_'); // 数据表前缀，使用Model方式调用API必须配置此项
