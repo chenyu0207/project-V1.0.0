8fa25be9a7fd3450cfbf24df1eb9a67b14a62876
chenyu <chenyu@chenyudeMacBook-Pro.local>
2018-06-20 18:36:13 +0800

    页面提交

---
+++ www/Application/Admin/Controller/ProjectController.class.php
@@ -0,0 +1,37 @@
+<?php
+// +----------------------------------------------------------------------
+// | OneThink [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013 http://www.onethink.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: chenyu
+// +----------------------------------------------------------------------
+
+namespace Admin\Controller;
+
+/**
+ * 物流后台管理
+ * @author chenyu
+ */
+
+class ProjectController extends AdminController
+{
+
+    /**
+     * 物流后台主页
+     * @author chenyu
+     */
+    public function index()
+    {
+        $this->display();
+    }
+
+    /**
+     * 物流数据新增
+     * @author chenyu
+     */
+    public function add()
+    {
+        $this->display();
+    }
+}
