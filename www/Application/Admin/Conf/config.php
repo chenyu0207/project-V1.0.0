03a5cea6af0a7904022c06c644c487e6340b01a1
47.105.99.15 <cuyc0207@163.com>
2018-09-18 17:11:58 +0800

    tiaoshi

--- www/Application/Admin/Conf/config.php
+++ www/Application/Admin/Conf/config.php
@@ -101,4 +101,6 @@ return array(
     'TMPL_ACTION_SUCCESS'   =>  MODULE_PATH.'View/Public/success.html', // 默认成功跳转对应的模板文件
     'TMPL_EXCEPTION_FILE'   =>  MODULE_PATH.'View/Public/exception.html',// 异常页面的模板文件
 
+    /* 调试配置 */
+    'SHOW_PAGE_TRACE' => false,
 );
