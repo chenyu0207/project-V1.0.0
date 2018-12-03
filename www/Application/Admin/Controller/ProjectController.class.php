092b2c72a60ac6fa3a8127c17b508438fa60d3bb
liuchang <63905589@qq.com>
2018-12-03 16:10:52 +0800

    修改生成pdf的高度和宽度：100*150

--- www/Application/Admin/Controller/ProjectController.class.php
+++ www/Application/Admin/Controller/ProjectController.class.php
@@ -264,13 +264,14 @@ class ProjectController extends AdminController
         require_once ORIGIN_PATH . '/vendor/autoload.php';
         $mpdf = new \Mpdf\Mpdf(
             [
-                ' margin_left '  =>  0,
-                ' margin_right '  =>  0,
-                ' margin_top '  =>  0,
-                ' margin_bottom '  =>  0,
-                ' margin_header '  =>  0,
-                ' margin_footer '  =>  0,
-                ' showBarcodeNumbers '  =>  true
+                'format' => [100, 150],
+                'margin_left'  =>  0,
+                'margin_right'  =>  0,
+                'margin_top'  =>  0,
+                'margin_bottom'  =>  0,
+                'margin_header'  =>  0,
+                'margin_footer'  =>  0,
+                'showBarcodeNumbers'  =>  true
             ]
         );
         echo $data;
